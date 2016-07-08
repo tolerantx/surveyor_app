64c6a97979c4c392148618e0bc16609c951c0d05
Abel Sierra <tolerantx@gmail.com>
2016-07-07 22:40:17 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SurveyorApp::Application.config.secret_key_base = '3dd028e79cb4e0f26afb29f49ab153b75819b67fe9657ae2f70e07aad3dbc845aecc508673346f6edd9bb197fe6a4f77087f4ae28d5ed04d661d1755d1f4ba84'
